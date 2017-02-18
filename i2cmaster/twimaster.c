4a49747e25a1bcddc38e5f0fbbd9b78255f0429b
Eric Tang <e_l_tang@outlook.com>
2017-02-18 06:48:42 -0800

    Slow down the I2C communication just a bit

--- i2cmaster/twimaster.c
+++ i2cmaster/twimaster.c
@@ -18,7 +18,7 @@
 #endif
 
 /* I2C clock in Hz */
-#define SCL_CLOCK  100000L
+#define SCL_CLOCK  400000L
 
 
 /*************************************************************************
@@ -29,7 +29,7 @@ void i2c_init(void)
   /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
 
   TWSR = 0;                         /* no prescaler */
-  TWBR = 10;  /* must be > 10 for stable operation */
+  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
 
 }/* i2c_init */
 
