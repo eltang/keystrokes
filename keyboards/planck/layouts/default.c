597ba7f7e9a30396f6fab1dd292798c52550c00b
Eric Tang <e_l_tang@outlook.com>
2017-02-22 16:41:34 -0800

    Update the default Planck layout

--- keyboards/planck/layouts/default.c
+++ keyboards/planck/layouts/default.c
@@ -11,13 +11,13 @@ LAYOUT(
         K(SC(US, '~')), K(SC(US, '!')), K(SC(US, '@')), K(SC(US, '#')), K(SC(US, '$')), K(SC(US, '%')), K(SC(US, '^')), K(SC(US, '&')), K(SC(US, '*')), K(SC(US, '(')), K(SC(US, ')')), K(SC(US, BKSP)),
         K(SC(US, DELETE)), K(SC(US, F1)), K(SC(US, F2)), K(SC(US, F3)), K(SC(US, F4)), K(SC(US, F5)), K(SC(US, F6)), K(SC(US, '_')), K(SC(US, '+')), K(SC(US, '{')), K(SC(US, '}')), K(SC(US, '|')),
         K(SC(US, L_SHIFT)), K(SC(US, F7)), K(SC(US, F8)), K(SC(US, F9)), K(SC(US, F10)), K(SC(US, F11)), K(SC(US, F12)), NO_ACTION, NO_ACTION, K(SC(US, HOME)), K(SC(US, END)), K(SC(US, ENTER)),
-        RESET, K(SC(US, L_CTRL)), K(SC(US, L_ALT)), K(SC(US, L_GUI)), L(VISIT, 1), K(SC(US, ' ')), K(SC(US, ' ')), L(VISIT, 2), K(C(HID_KEYBOARD_SC_MEDIA_NEXT_TRACK)), K(C(HID_KEYBOARD_SC_MEDIA_VOLUME_DOWN)), K(C(HID_KEYBOARD_SC_MEDIA_VOLUME_UP)), K(C(HID_KEYBOARD_SC_MEDIA_PLAY))
+        RESET, K(SC(US, L_CTRL)), K(SC(US, L_ALT)), K(SC(US, L_GUI)), L(VISIT, 1), K(SC(US, ' ')), K(SC(US, ' ')), L(VISIT, 2), K(C(0xB5)), K(C(0xEA)), K(C(0xE9)), K(C(0xCD))
     },
     {
         K(SC(US, '`')), K(SC(US, '1')), K(SC(US, '2')), K(SC(US, '3')), K(SC(US, '4')), K(SC(US, '5')), K(SC(US, '6')), K(SC(US, '7')), K(SC(US, '8')), K(SC(US, '9')), K(SC(US, '0')), K(SC(US, BKSP)),
         K(SC(US, DELETE)), K(SC(US, F1)), K(SC(US, F2)), K(SC(US, F3)), K(SC(US, F4)), K(SC(US, F5)), K(SC(US, F6)), K(SC(US, '-')), K(SC(US, '=')), K(SC(US, '[')), K(SC(US, ']')), K(SC(US, '\\')),
         K(SC(US, L_SHIFT)), K(SC(US, F7)), K(SC(US, F8)), K(SC(US, F9)), K(SC(US, F10)), K(SC(US, F11)), K(SC(US, F12)), NO_ACTION, NO_ACTION, K(SC(US, PG_UP)), K(SC(US, PG_DN)), K(SC(US, ENTER)),
-        NO_ACTION, K(SC(US, L_CTRL)), K(SC(US, L_ALT)), K(SC(US, L_GUI)), L(VISIT, 1), K(SC(US, ' ')), K(SC(US, ' ')), L(VISIT, 2), K(C(HID_KEYBOARD_SC_MEDIA_NEXT_TRACK)), K(C(HID_KEYBOARD_SC_MEDIA_VOLUME_DOWN)), K(C(HID_KEYBOARD_SC_MEDIA_VOLUME_UP)), K(C(HID_KEYBOARD_SC_MEDIA_PLAY))
+        NO_ACTION, K(SC(US, L_CTRL)), K(SC(US, L_ALT)), K(SC(US, L_GUI)), L(VISIT, 1), K(SC(US, ' ')), K(SC(US, ' ')), L(VISIT, 2), K(C(0xB5)), K(C(0xEA)), K(C(0xE9)), K(C(0xCD))
     }
 );
 
