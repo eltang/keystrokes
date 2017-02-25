510edea6cbe9f4e6c00847ef5769071c854b67ad
Eric Tang <e_l_tang@outlook.com>
2017-02-25 14:21:44 -0800

    Allow HalfKay to run

--- main.c
+++ main.c
@@ -134,10 +134,6 @@ int main(void)
 void SetupHardware()
 {
 #if (ARCH == ARCH_AVR8)
-	/* Disable watchdog if enabled by bootloader/fuses */
-	MCUSR &= ~(1 << WDRF);
-	wdt_disable();
-
 	/* Disable clock division */
 	clock_prescale_set(clock_div_1);
     MCUCR |= 1 << JTD;
