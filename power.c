4d4522e432cb2662012545f1cf6acf626ac4e28b
Eric Tang <e_l_tang@outlook.com>
2017-05-09 23:43:51 -0700

    Clean up code which accesses the Power Reduction Registers

--- power.c
+++ power.c
@@ -7,25 +7,12 @@ static const __flash struct pin unused_pins[] = UNUSED_PINS;
 void power_init(void)
 {
     struct pin pin;
-    uint8_t temp = 0;
 
     for (uint8_t i = sizeof(unused_pins) / sizeof(unused_pins[0]); i--;) {
         pin = unused_pins[i];
 
         *pins_addr_to_reg(pin.addr, PORT) |= pin.mask;
     }
-#ifndef USE_TWI
-    temp |= 1 << PRTWI;
-#endif
-#ifndef USE_BACKLIGHT
-    temp |= 1 << PRTIM1;
-#endif
-    temp |= 1 << PRSPI;
-    temp |= 1 << PRADC;
-    PRR0 = temp;
-    temp = 0;
-    temp |= 1 << 4; // PRTIM4
-    temp |= 1 << PRTIM3;
-    temp |= 1 << PRUSART1;
-    PRR1 = temp;
+    PRR0 = PRR0_VALUE;
+    PRR1 = PRR1_VALUE;
 }
--- power.h
+++ power.h
@@ -3,4 +3,23 @@
 
 void power_init(void);
 
+#ifdef USE_TWI
+#define PRTWI_VALUE 0
+#else
+#define PRTWI_VALUE (1 << PRTWI)
+#endif
+#ifdef USE_BACKLIGHT
+#define PRTIM1_VALUE 0
+#else
+#define PRTIM1_VALUE (1 << PRTIM1)
+#endif
+#define PRSPI_VALUE (1 << PRSPI)
+#define PRADC_VALUE (1 << PRADC)
+#define PRTIM4_VALUE (1 << 4)
+#define PRTIM3_VALUE (1 << PRTIM3)
+#define PRUSART1_VALUE (1 << PRUSART1)
+
+#define PRR0_VALUE (PRTWI_VALUE | PRTIM1_VALUE | PRSPI_VALUE | PRADC_VALUE)
+#define PRR1_VALUE (PRTIM4_VALUE | PRTIM3_VALUE | PRUSART1_VALUE)
+
 #endif
