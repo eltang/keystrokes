95d1804ceb4e066906e933995576afa965c9436e
Eric Tang <e_l_tang@outlook.com>
2017-05-12 17:00:50 -0700

    Modify config.h for the ErgoDox

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
-// #define USE_TWI
-// #define BACKWARDS_DIODES
-#define ROWS 4
-#define COLUMNS 12
+#define USE_TWI
+#define BACKWARDS_DIODES
+#define ROWS 6
+#define COLUMNS 14
 #define MAX_TAP_DURATION 250
-#define ROW_PINS { PIN(D, 0), PIN(D, 5), PIN(B, 5), PIN(B, 6) }
-#define COLUMN_PINS { PIN(F, 1), PIN(F, 0), PIN(B, 0), PIN(C, 7), PIN(F, 4), PIN(F, 5), PIN(F, 6), PIN(F, 7), PIN(D, 4), PIN(D, 6), PIN(B, 4), PIN(D, 7)  }
-#define BOOTLOADER_SEC_SIZE_BYTES 4096
-#define LED_CONFIG { }
+#define ROW_PINS { }
+#define COLUMN_PINS { }
+#define BOOTLOADER_SEC_SIZE_BYTES 512
+#define LED_CONFIG { { PIN(B, 5), SHOW_CAPS_LOCK_STATE }, { PIN(B, 6), SHOW_LAYER_BIT_1 }, { PIN(B, 7), SHOW_LAYER_BIT_0 } }
 #define USE_VIRTUAL_SERIAL
